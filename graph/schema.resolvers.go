d473b75bb47834cce664e82485e546c5a8b29dc3
xStrato <metalofgear@hotmail.com>
2021-12-26 16:02:29 -0300

    update

--- graph/schema.resolvers.go
+++ graph/schema.resolvers.go
@@ -6,33 +6,66 @@ package graph
 import (
 	"context"
 	"fmt"
+	"math/rand"
 
 	"github.com/xStrato/graphql-golang-sample/graph/generated"
 	"github.com/xStrato/graphql-golang-sample/graph/model"
 )
 
 func (r *mutationResolver) CreateCategory(ctx context.Context, input model.NewCategory) (*model.Category, error) {
-	panic(fmt.Errorf("not implemented"))
+	model := &model.Category{
+		ID:          fmt.Sprintf("T%d", rand.Int()),
+		Name:        input.Name,
+		Description: &input.Description,
+	}
+	r.Resolver.Categories = append(r.Resolver.Categories, model)
+	return model, nil
 }
 
 func (r *mutationResolver) CreateCourse(ctx context.Context, input model.NewCourse) (*model.Course, error) {
-	panic(fmt.Errorf("not implemented"))
+
+	var category *model.Category
+	for _, v := range r.Categories {
+		if v.ID == input.CategoryID {
+			category = v
+		}
+	}
+	model := &model.Course{
+		ID:          fmt.Sprintf("T%d", rand.Int()),
+		Name:        input.Name,
+		Description: &input.Description,
+		Category:    category,
+	}
+	r.Resolver.Courses = append(r.Resolver.Courses, model)
+	return model, nil
 }
 
 func (r *mutationResolver) CreateChapter(ctx context.Context, input model.NewChapter) (*model.Chapter, error) {
-	panic(fmt.Errorf("not implemented"))
+	var course *model.Course
+	for _, v := range r.Courses {
+		if v.ID == input.CourseID {
+			course = v
+		}
+	}
+	model := &model.Chapter{
+		ID:     fmt.Sprintf("T%d", rand.Int()),
+		Name:   input.Name,
+		Course: course,
+	}
+	r.Resolver.Chapters = append(r.Resolver.Chapters, model)
+	return model, nil
 }
 
 func (r *queryResolver) Courses(ctx context.Context) ([]*model.Course, error) {
-	panic(fmt.Errorf("not implemented"))
+	return r.Resolver.Courses, nil
 }
 
 func (r *queryResolver) Categories(ctx context.Context) ([]*model.Category, error) {
-	panic(fmt.Errorf("not implemented"))
+	return r.Resolver.Categories, nil
 }
 
 func (r *queryResolver) Chapters(ctx context.Context) ([]*model.Chapter, error) {
-	panic(fmt.Errorf("not implemented"))
+	return r.Resolver.Chapters, nil
 }
 
 // Mutation returns generated.MutationResolver implementation.
